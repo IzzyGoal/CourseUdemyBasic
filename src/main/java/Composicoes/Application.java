dde28ce8900d40bad0dd32165e1c9f27a8e790c8
Gabriel Silva Lima <gabriel.osilvalima@gmail.com>
2021-05-18 15:54:15 -0300

    Trabalhando composicao

---
+++ src/main/java/Composicoes/Application.java
@@ -0,0 +1,7 @@
+package Composicoes;
+
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/Composicoes/entities/Enums/WorkerLevel.java
@@ -0,0 +1,7 @@
+package Composicoes.entities.Enums;
+
+public enum WorkerLevel {
+    JUNIOR,
+    MID_LEVEL,
+    SENIOR;
+}
