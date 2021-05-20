8c517ad1d6ba492c6661589a05fd82b7c8403e81
Gabriel Silva Lima <gabriel.osilvalima@gmail.com>
2021-05-20 12:56:01 -0300

    começo de poliformismo

---
+++ src/main/java/herancaPolimorfismo/entities/Employee.java
@@ -0,0 +1,45 @@
+package herancaPolimorfismo.entities;
+
+public class Employee {
+    private String name;
+    private Integer hour;
+    private double valuePerHour;
+
+    public Employee(){
+
+    }
+
+    public Employee(String name, Integer hour, double valuePerHour) {
+        this.name = name;
+        this.hour = hour;
+        this.valuePerHour = valuePerHour;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getHour() {
+        return hour;
+    }
+
+    public void setHour(Integer hour) {
+        this.hour = hour;
+    }
+
+    public double getValuePerHour() {
+        return valuePerHour;
+    }
+
+    public void setValuePerHour(double valuePerHour) {
+        this.valuePerHour = valuePerHour;
+    }
+    public double payment(){
+        return hour * valuePerHour;
+    }
+}
+
---
+++ src/main/java/herancaPolimorfismo/program.java
@@ -0,0 +1,4 @@
+package herancaPolimorfismo;
+
+public class program {
+}
